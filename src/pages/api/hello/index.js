61e3e6413ad855f49e5a3e36a5c1a0de5cc64b64
ngmthaq <nguyenmanhthang2000.fb@gmail.com>
2022-11-20 11:15:40 +0700

    Update api structure

--- src/pages/api/hello/index.js
+++ src/pages/api/hello/index.js
@@ -2,13 +2,19 @@
 
 import { HelloRepo } from "../../../repo";
 
-export default async function handler(req, res) {
+const handler = async (req, res) => {
   if (req.method === "GET") {
-    const users = await HelloRepo.getUsers();
-    res.status(200).json(users);
+    return getUsers(req, res);
   } else if (req.method === "POST") {
     res.status(200).json({ message: "POST METHOD" });
   } else {
     res.status(200).json({ message: "ANOTHER METHOD" });
   }
-}
+};
+
+const getUsers = async (req, res) => {
+  const users = await HelloRepo.getUsers();
+  res.status(200).json(users);
+};
+
+export default handler;
